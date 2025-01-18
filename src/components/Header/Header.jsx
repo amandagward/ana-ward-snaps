0ae45f27633669fda9dfc5916b22bc6415061839
Ana Ward <amandagward@me.com>
2025-01-17 19:04:35 -0500

    initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import "./Header.scss";
+
+function Header() {
+    return (
+        <header>
+            <div className="header">
+                <h1>Snaps</h1>
+            </div>
+        </header>
+    );
+}
+
+export default Header
